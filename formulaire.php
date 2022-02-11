f6a8124480c5da4109a5311a658b9e44f9a8f237
Bils78 <99476270+Bils78@users.noreply.github.com>
2022-02-11 16:51:04 +0100

    Add files via upload

---
+++ formulaire.php
@@ -0,0 +1,12 @@
+
+<form action="action2.php" method="post">
+<p>Votre nom : <input type="text" name="nom" /></p>
+<p>Votre âge : <input type="text" name="age" /></p>
+<p>Votre prénom : <input type="text" name="prenom" /></p>
+<p>Votre ville : <input type="text" name="ville" /></p>
+<p>Votre adresse : <input type="text" name="adresse" /></p>
+<p>Votre Code postal : <input type="text" name="codep" /></p>
+<p><input type="submit" value="OK"></p>
+</form>
+
+
